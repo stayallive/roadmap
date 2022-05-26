4691bf9dfbd4921e5cb6f82453c2a83f8a2042f9
Alex Bouma <alex@bouma.me>
2022-05-26 17:33:35 +0200

    wip

--- database/migrations/2022_05_11_164916_add_item_user_table.php
+++ database/migrations/2022_05_11_164916_add_item_user_table.php
@@ -13,10 +13,9 @@ return new class extends Migration {
     public function up()
     {
         Schema::create('item_user', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('item_id')->nullable()->constrained();
             $table->foreignId('user_id')->nullable()->constrained();
-
-            $table->primary(['item_id', 'user_id']);
         });
     }
 
